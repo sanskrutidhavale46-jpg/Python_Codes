9fa7ce277d6d0ec289f7cec4dc0212acf69e23c1
Sanskruti Dhavale <sanskrutidhavale46@gmail.com>
2026-07-30 22:34:30 +0530

    update file program1.py

---
+++ program1.py
@@ -0,0 +1,24 @@
+# variables
+name="sanskruti dhavale"
+age=19
+price=25.99
+print(name)
+print(age)
+print(price)
+
+#variable 1
+name="sanskruti dhavale"
+age=19
+
+print("my name is:",name)
+print("my age is:",age)
+
+#identifiers
+
+name="sanskruti"
+age=19
+price=25.99
+
+print(type(name))
+print(type(age))
+print(type(price))
